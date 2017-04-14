92ecf5de8eaea3a4a9f9789dcebcf65218afec9f
alexandrebl <abrandao@stone.com.br>
2017-04-14 11:15:47 -0300

    fisr commit

---
+++ UtilityHelperTools/Utilities/Interfaces/IDataTableUtility.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Data;
+
+namespace UtilityHelperTools.Utilities.Interfaces {
+
+    /// <summary>
+    /// Interface de dados
+    /// </summary>
+    public interface IDataTableUtility {
+
+        /// <summary>
+        /// Utilitário de conversão
+        /// </summary>
+        /// <typeparam name="T">tipo</typeparam>
+        /// <param name="data">dados</param>
+        /// <returns></returns>
+        DataTable AsDataTable<T>(IEnumerable<T> data);
+    }
+}
